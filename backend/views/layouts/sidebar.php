7aa001953a0258cec37d2092fce3d732443d1f29
Diogo Lopes <2211850@my.ipleiria.pt>
2022-11-10 21:40:22 +0000

    Revert "Merge branch 'EmpresaController' into develop"
    This reverts commit 931455d99388cc1b05696f3ed9d16adc2fe5abef.

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -40,8 +40,8 @@
                         'icon' => 'tachometer-alt',
                         'badge' => '<span class="right badge badge-info">2</span>',
                         'items' => [
-                            ['label' => 'Active Page', 'url' => ['site/index'], 'iconStyle' => 'far'],
-                            ['label' => 'Inactive Page', 'iconStyle' => 'far'],
+                            ['label' => 'Gerir Lojas', 'url' => ['loja/index'], 'iconStyle' => 'far'],
+                            ['label' => 'Gerir Utilizadores', 'iconStyle' => 'far'],
                         ]
                     ],
                     ['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
