4764bad55726720bc292b319c4429f55fb819284
Fernando José Videira <2211877@my.ipleiria.pt>
2023-01-06 19:25:10 +0000

    Carrinho Test Added

---
+++ frontend/tests/unit/CarrinhoTest.php
@@ -0,0 +1,181 @@
+<?php
+
+
+namespace frontend\tests\Unit;
+
+use common\models\Carrinho;
+use common\models\Linhacarrinho;
+use common\models\Morada;
+use common\models\Promocao;
+use common\models\User;
+use common\models\Utilizador;
+use frontend\tests\UnitTester;
+
+class CarrinhoTest extends \Codeception\Test\Unit
+{
+
+    protected UnitTester $tester;
+
+
+    // tests
+    public function testValidations()
+    {
+        //Create User entry on database
+        $user = new User();
+        $user->username = 'fernando';
+        $user->email = 'fernandoj.videira@pm.me';
+        $user->password = 'okmokmokm';
+        $user->generateAuthKey();
+        $user->save();
+
+        //Create Utilizador entry on database
+        $utilizador = new Utilizador();
+        $utilizador->setAttribute('nome', 'Fernando');
+        $utilizador->setAttribute('nif', '123456789');
+        $utilizador->setAttribute('telemovel', '123456789');
+        $utilizador->setAttribute('id_user', $user->id);
+        $utilizador->save();
+
+        //Create Morada entry on database
+        $morada = new Morada();
+        $morada->setAttribute('rua', 'Ruas');
+        $morada->setAttribute('cidade','123');
+        $morada->setAttribute('cod_postal','1232');
+        $morada->setAttribute('pais','Portugal');
+        $morada->save();
+
+        //Create Promoção entry on database
+        $promocao = new Promocao();
+        $promocao->setAttribute('nome_promo', 'Teste');
+        $promocao->setAttribute('percentagem', 10);
+        $promocao->setAttribute('data_limite', '2021-01-01');
+        $promocao->setAttribute('codigo', '12345');
+        $promocao->save();
+
+
+
+        //Carrinho
+        $carrinho = new Carrinho();
+        $morada = $this->tester->grabRecord('common\models\Morada', ['rua' => 'Ruas']);
+        $promo = $this->tester->grabRecord('common\models\Promocao', ['nome_promo' => 'Teste']);
+        $usr = $this->tester->grabRecord('common\models\Utilizador', ['nome' => 'Fernando']);
+
+        $carrinho->setAttribute('id_morada', 2); //Morada com id 2 não existe na base de dados
+        $this->assertFalse($carrinho->validate(['id_morada']));
+
+        $carrinho->setAttribute('id_morada', $morada->idMorada);
+        $this->assertTrue($carrinho->validate(['id_morada']));
+
+        //O valor do id_morada pode ser null enquanto o utilizador não "fecha" o carrinho
+        $carrinho->setAttribute('id_morada', null);
+        $this->assertTrue($carrinho->validate(['id_morada']));
+
+        $carrinho->setAttribute('data_criacao', '2020/01/01');
+        $this->assertFalse($carrinho->validate(['data_criacao']));
+
+        $carrinho->setAttribute('data_criacao', null);
+        $this->assertFalse($carrinho->validate(['data_criacao']));
+
+        $carrinho->setAttribute('data_criacao', '2022-01-01 12:00:00');
+        $this->assertTrue($carrinho->validate(['data_criacao']));
+
+        $carrinho->setAttribute('id_user', 5); //Utilizador com id 5 não existe na base de dados
+        $this->assertFalse($carrinho->validate(['id_user']));
+
+        $carrinho->setAttribute('id_user', $usr->idUser);
+        $this->assertTrue($carrinho->validate(['id_user']));
+
+        //O valor do id_user pode ser null enquanto o utilizador é "Guest" e não se encontra logado
+        $carrinho->setAttribute('id_user', null);
+        $this->assertTrue($carrinho->validate(['id_user']));
+
+        $carrinho->setAttribute('id_promocao', 10); //Promoção com id 5 não existe na base de dados
+        $this->assertFalse($carrinho->validate(['id_promocao']));
+
+        $carrinho->setAttribute('id_promocao', $promo->idPromocao);
+        $this->assertTrue($carrinho->validate(['id_promocao']));
+
+        //O valor do id_promo pode ser null enquanto o utilizador não inserir um código de promoção
+        $carrinho->setAttribute('id_promocao', null);
+        $this->assertTrue($carrinho->validate(['id_promocao']));
+
+        $carrinho->setAttribute('estado', 'Pendente');
+        $this->assertFalse($carrinho->validate(['estado']));
+
+        $carrinho->setAttribute('estado', null);
+        $this->assertFalse($carrinho->validate(['estado']));
+
+        //O estado apenas aceita estes 3 valores
+        $carrinho->setAttribute('estado', 'aberto');
+        $this->assertTrue($carrinho->validate(['estado']));
+
+        $carrinho->setAttribute('estado', 'fechado');
+        $this->assertTrue($carrinho->validate(['estado']));
+
+        $carrinho->setAttribute('estado', 'emProcessamento');
+        $this->assertTrue($carrinho->validate(['estado']));
+
+    }
+
+    public function testSaveUpdateDelete()
+    {
+        //Create User entry on database
+        $user = new User();
+        $user->username = 'fernando';
+        $user->email = 'fernandoj.videira@pm.me';
+        $user->password = 'okmokmokm';
+        $user->generateAuthKey();
+        $user->save();
+
+        //Create Utilizador entry on database
+        $utilizador = new Utilizador();
+        $utilizador->setAttribute('nome', 'Fernando');
+        $utilizador->setAttribute('nif', '123456789');
+        $utilizador->setAttribute('telemovel', '123456789');
+        $utilizador->setAttribute('id_user', $user->id);
+        $utilizador->save();
+
+        //Create Morada entry on database
+        $morada = new Morada();
+        $morada->setAttribute('rua', 'Ruas');
+        $morada->setAttribute('cidade','123');
+        $morada->setAttribute('cod_postal','1232');
+        $morada->setAttribute('pais','Portugal');
+        $morada->save();
+
+        //Create Promoção entry on database
+        $promocao = new Promocao();
+        $promocao->setAttribute('nome_promo', 'Teste');
+        $promocao->setAttribute('percentagem', 10);
+        $promocao->setAttribute('data_limite', '2021-01-01');
+        $promocao->setAttribute('codigo', '12345');
+        $promocao->save();
+
+
+        //Test Save
+        $carrinho = new Carrinho();
+
+        $carrinho->idCarrinho = 1; //para facilitar a visualização do teste
+        $carrinho->estado = 'aberto';
+        $carrinho->data_criacao = '2021-01-01 12:00:00';
+        $carrinho->id_user = $utilizador->idUser;
+        $carrinho->save();
+
+        $this->tester->seeRecord('common\models\Carrinho', ['idCarrinho' => 1]);
+
+        //Test Update
+        $carrinho = $this->tester->grabRecord('common\models\Carrinho', ['idCarrinho' => 1]);
+        $carrinho->estado = 'emProcessamento';
+        $carrinho->save();
+
+        $this->tester->seeRecord('common\models\Carrinho', ['idCarrinho' => 1, 'estado' => 'emProcessamento']);
+        $this->tester->dontSeeRecord('common\models\Carrinho', ['idCarrinho' => 1, 'estado' => 'aberto']);
+
+        //Test Delete
+        $carrinho->delete();
+        $this->tester->dontSeeRecord('common\models\Carrinho', ['idCarrinho' => 1]);
+
+
+    }
+
+}
